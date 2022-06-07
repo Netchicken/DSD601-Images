b62e03b8b0dba0f077e6e511dc409ac1f5392a5a
Gary <Gary.Dix@enform.nz>
2022-06-08 11:48:15 +1200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dsd601Images2020.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+
+        public string? Imagepath { get; set; }
+        //   public string? FinalImagepath { get; set; }
+
+        public List<string>? ImagepathList;
+
+
+
+        public IndexModel()
+        {
+            ImagepathList = new List<string>();
+
+            ImagepathList.Add("photo1.jpg");
+            ImagepathList.Add("photo2.jpg");
+            ImagepathList.Add("photo3.jpg");
+            ImagepathList.Add("photo4.jpg");
+            ImagepathList.Add("photo5.jpg");
+            ImagepathList.Add("photo6.jpg");
+
+
+        }
+
+        public void OnGet()
+        {
+
+
+        }
+
+
+        public void OnPost()
+        {
+            // FinalImagepath = Imagepath;
+
+        }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dsd601Images2020.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
