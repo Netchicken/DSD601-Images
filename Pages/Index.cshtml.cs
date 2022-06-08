18306619b2a16477b3968117b4c28b7c5e50fb2e
Gary <Gary.Dix@enform.nz>
2022-06-09 09:51:34 +1200

    formatting

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,22 +6,25 @@ namespace dsd601Images2020.Pages
     [BindProperties]
     public class IndexModel : PageModel
     {
-
+        //the selected image
         public string? Imagepath { get; set; }
+        //a list of images
         public List<string>? ImagepathList;
 
 
-
+        //create the constructor by typing ctor
         public IndexModel()
         {
-            ImagepathList = new List<string>();
-
-            ImagepathList.Add("photo1.jpg");
-            ImagepathList.Add("photo2.jpg");
-            ImagepathList.Add("photo3.jpg");
-            ImagepathList.Add("photo4.jpg");
-            ImagepathList.Add("photo5.jpg");
-            ImagepathList.Add("photo6.jpg");
+            ImagepathList = new List<string>
+            {
+                //image names added to the list
+                "photo1.jpg",
+                "photo2.jpg",
+                "photo3.jpg",
+                "photo4.jpg",
+                "photo5.jpg",
+                "photo6.jpg"
+            };
         }
 
         public void OnGet()
