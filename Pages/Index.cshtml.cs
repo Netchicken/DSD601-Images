ac2bcd7b614e10d2e5ff306ed177d36bae9eed99
Gary Dix <intelproof@gmail.com>
2025-03-25 09:38:33 +1300

    created loadimages as a pure method

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,12 +9,12 @@ namespace dsd601Images2020.Pages
         //the selected image
         public string? Imagepath { get; set; }
         //a list of images
-        public List<string>? ImagepathList;
+        public List<string>? ImagepathList = new List<string>();
 
 
         public void OnGet()
         {
-            LoadImage();
+            ImagepathList.AddRange(LoadImagePure());
         }
 
 
@@ -26,7 +26,7 @@ namespace dsd601Images2020.Pages
         private void LoadImage()
         {
             //manually add files to the list
-            ImagepathList = new List<string>();
+            //  ImagepathList = new List<string>();
             //{
             //    //image names added to the list
             //    "photo1.jpg",
@@ -55,6 +55,38 @@ namespace dsd601Images2020.Pages
                 string fileName = Path.GetRelativePath("wwwroot\\images", file); //this works as well.
                 ImagepathList.Add(fileName);
             }
+
+
         }
+
+
+
+        private List<string> LoadImagePure()
+        {
+            //manually add files to the list
+            List<string>? imagepathList = new List<string>();
+
+
+            // Get the path to the wwwroot folder
+            string wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images");
+
+            // Get all files in the wwwroot folder
+            string[] allFiles = Directory.GetFiles(wwwrootPath, "*.*", SearchOption.AllDirectories);
+
+            // Print the names of the files
+            foreach (string file in allFiles)
+            {
+                //remove the path to the image leaving only the name
+                // string fileName = Path.GetFileName(file); //get the file name only
+
+                //get the relative path to the image
+                string fileName = Path.GetRelativePath("wwwroot\\images", file); //this works as well.
+                imagepathList.Add(fileName);
+            }
+
+            return imagepathList;
+        }
+
+
     }
 }
