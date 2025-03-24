ebcea6f127d93498a1d4fdd95268758f9664a1df
Gary Dix <intelproof@gmail.com>
2025-03-24 15:20:46 +1300

    Added directory getfiles to load all images in folder programatically.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -15,17 +15,39 @@ namespace dsd601Images2020.Pages
         //create the constructor by typing ctor
         public IndexModel()
         {
-            ImagepathList = new List<string>
-            {
-                //image names added to the list
-                "photo1.jpg",
-                "photo2.jpg",
-                "photo3.jpg",
-                "photo4.jpg",
-                "photo5.jpg",
-                "photo6.jpg"
-            };
+            ImagepathList = new List<string>();
+            //{
+            //    //image names added to the list
+            //    "photo1.jpg",
+            //    "photo2.jpg",
+            //    "photo3.jpg",
+            //    "photo4.jpg",
+            //    "photo5.jpg",
+            //    "photo6.jpg"
+            //};
+
+            LoadImage();
         }
+
+
+        private void LoadImage()
+        {
+            // Get the path to the wwwroot folder
+            string wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images");
+
+            // Get all files in the wwwroot folder
+            string[] allFiles = Directory.GetFiles(wwwrootPath, "*.*", SearchOption.AllDirectories);
+
+            // Print the names of the files
+            foreach (string file in allFiles)
+            {
+                //remove the path to the image leaving only the name
+                string fileName = Path.GetFileName(file);
+                ImagepathList.Add(fileName);
+            }
+        }
+
+
 
         public void OnGet()
         {
